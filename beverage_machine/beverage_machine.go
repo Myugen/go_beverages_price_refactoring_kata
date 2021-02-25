6e1bcdeb7882de57356cb33c022de5fefea1a31f
Miguel Cabrera <miguelcabsan@gmail.com>
2021-02-25 00:08:34 +0000

    refactor(beverages): Refactor beverage machine to use makers

--- beverage_machine/beverage_machine.go
+++ beverage_machine/beverage_machine.go
@@ -1,49 +1,31 @@
 package beverage_machine
 
-import (
-	"github.com/myugen/go_beverages_price_refactoring_kata/beverages"
-	"github.com/myugen/go_beverages_price_refactoring_kata/beverages/supplements"
-)
+import "github.com/myugen/go_beverages_price_refactoring_kata/beverage_machine/makers"
 
 type BeverageOption int16
 
-const (
-	BeverageOptionCoffee BeverageOption = iota + 1
-	BeverageOptionTea
-	BeverageOptionHotChocolate
-)
-
 type BeverageMachine struct {
-	beverage beverages.Beverage
+	coffeeMaker       *makers.CoffeeMaker
+	hotChocolateMaker *makers.HotChocolateMaker
+	teaMaker          *makers.TeaMaker
 }
 
-func (b *BeverageMachine) WithMilk() *BeverageMachine {
-	b.beverage = supplements.NewWithMilk(b.beverage)
-	return b
+func (b *BeverageMachine) Coffee() *makers.CoffeeMaker {
+	return b.coffeeMaker
 }
 
-func (b *BeverageMachine) WithCream() *BeverageMachine {
-	b.beverage = supplements.NewWithCream(b.beverage)
-	return b
+func (b *BeverageMachine) Tea() *makers.TeaMaker {
+	return b.teaMaker
 }
 
-func (b *BeverageMachine) WithCinnamon() *BeverageMachine {
-	b.beverage = supplements.NewWithCinnamon(b.beverage)
-	return b
+func (b *BeverageMachine) HotChocolate() *makers.HotChocolateMaker {
+	return b.hotChocolateMaker
 }
 
-func (b *BeverageMachine) Make() beverages.Beverage {
-	return b.beverage
-}
-
-func NewBeverageMachine(option BeverageOption) *BeverageMachine {
-	switch option {
-	case BeverageOptionCoffee:
-		return &BeverageMachine{beverage: beverages.NewCoffee()}
-	case BeverageOptionTea:
-		return &BeverageMachine{beverage: beverages.NewTea()}
-	case BeverageOptionHotChocolate:
-		return &BeverageMachine{beverage: beverages.NewHotChocolate()}
+func NewBeverageMachine() *BeverageMachine {
+	return &BeverageMachine{
+		coffeeMaker:       makers.NewCoffeeMaker(),
+		hotChocolateMaker: makers.NewHotChocolateMaker(),
+		teaMaker:          makers.NewTeaMaker(),
 	}
-	panic("Option not available")
 }
--- beverage_machine/beverages_pricing_test.go
+++ beverage_machine/beverages_pricing_test.go
@@ -15,97 +15,97 @@ const precision = 0.001
 func TestCoffee_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var coffee beverages.Beverage
-	coffee = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionCoffee).Make()
+	coffee = beverage_machine.NewBeverageMachine().Coffee().Make()
 	assertt.InDelta(1.2, coffee.Price(), precision)
 }
 
 func TestCoffeeWithCinnamon_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var coffeeWithCinnamon beverages.Beverage
-	coffeeWithCinnamon = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionCoffee).WithCinnamon().Make()
+	coffeeWithCinnamon = beverage_machine.NewBeverageMachine().Coffee().WithCinnamon().Make()
 	assertt.InDelta(1.25, coffeeWithCinnamon.Price(), precision)
 }
 
 func TestCoffeeWithMilk_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var coffeeWithMilk beverages.Beverage
-	coffeeWithMilk = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionCoffee).WithMilk().Make()
+	coffeeWithMilk = beverage_machine.NewBeverageMachine().Coffee().WithMilk().Make()
 	assertt.InDelta(1.3, coffeeWithMilk.Price(), precision)
 }
 
 func TestCoffeeWithMilkAndCinnamon_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var coffeeWithMilkAndCinnamon beverages.Beverage
-	coffeeWithMilkAndCinnamon = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionCoffee).WithMilk().WithCinnamon().Make()
+	coffeeWithMilkAndCinnamon = beverage_machine.NewBeverageMachine().Coffee().WithMilk().WithCinnamon().Make()
 	assertt.InDelta(1.35, coffeeWithMilkAndCinnamon.Price(), precision)
 }
 
 func TestCoffeeWithMilkAndCream_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var coffeeWithMilkAndCream beverages.Beverage
-	coffeeWithMilkAndCream = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionCoffee).WithMilk().WithCream().Make()
+	coffeeWithMilkAndCream = beverage_machine.NewBeverageMachine().Coffee().WithMilk().WithCream().Make()
 	assertt.InDelta(1.45, coffeeWithMilkAndCream.Price(), precision)
 }
 
 func TestCoffeeWithMilkAndCreamAndCinnamon_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var coffeeWithMilkAndCreamAndCinnamon beverages.Beverage
-	coffeeWithMilkAndCreamAndCinnamon = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionCoffee).WithMilk().WithCream().WithCinnamon().Make()
+	coffeeWithMilkAndCreamAndCinnamon = beverage_machine.NewBeverageMachine().Coffee().WithMilk().WithCream().WithCinnamon().Make()
 	assertt.InDelta(1.5, coffeeWithMilkAndCreamAndCinnamon.Price(), precision)
 }
 
 func TestTea_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var tea beverages.Beverage
-	tea = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionTea).Make()
+	tea = beverage_machine.NewBeverageMachine().Tea().Make()
 	assertt.InDelta(1.5, tea.Price(), precision)
 }
 
 func TestTeaWithCinnamon_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var teaWithCinnamon beverages.Beverage
-	teaWithCinnamon = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionTea).WithCinnamon().Make()
+	teaWithCinnamon = beverage_machine.NewBeverageMachine().Tea().WithCinnamon().Make()
 	assertt.InDelta(1.55, teaWithCinnamon.Price(), precision)
 }
 
 func TestTeaWithMilk_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var teaWithMilk beverages.Beverage
-	teaWithMilk = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionTea).WithMilk().Make()
+	teaWithMilk = beverage_machine.NewBeverageMachine().Tea().WithMilk().Make()
 	assertt.InDelta(1.6, teaWithMilk.Price(), precision)
 }
 
 func TestTeaWithMilkAndCinnamon_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var teaWithMilkAndCinnamon beverages.Beverage
-	teaWithMilkAndCinnamon = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionTea).WithMilk().WithCinnamon().Make()
+	teaWithMilkAndCinnamon = beverage_machine.NewBeverageMachine().Tea().WithMilk().WithCinnamon().Make()
 	assertt.InDelta(1.65, teaWithMilkAndCinnamon.Price(), precision)
 }
 
 func TestHotChocolate_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var hotChocolate beverages.Beverage
-	hotChocolate = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionHotChocolate).Make()
+	hotChocolate = beverage_machine.NewBeverageMachine().HotChocolate().Make()
 	assertt.InDelta(1.45, hotChocolate.Price(), precision)
 }
 
 func TestHotChocolateWithCinnamon_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var hotChocolateWithCinnamon beverages.Beverage
-	hotChocolateWithCinnamon = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionHotChocolate).WithCinnamon().Make()
+	hotChocolateWithCinnamon = beverage_machine.NewBeverageMachine().HotChocolate().WithCinnamon().Make()
 	assertt.InDelta(1.5, hotChocolateWithCinnamon.Price(), precision)
 }
 
 func TestHotChocolateWithCream_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var hotChocolateWithCream beverages.Beverage
-	hotChocolateWithCream = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionHotChocolate).WithCream().Make()
+	hotChocolateWithCream = beverage_machine.NewBeverageMachine().HotChocolate().WithCream().Make()
 	assertt.InDelta(1.6, hotChocolateWithCream.Price(), precision)
 }
 
 func TestHotChocolateWithCreamAndCinnamon_Price(t *testing.T) {
 	assertt := assert.New(t)
 	var hotChocolateWithCreamAndCinnamon beverages.Beverage
-	hotChocolateWithCreamAndCinnamon = beverage_machine.NewBeverageMachine(beverage_machine.BeverageOptionHotChocolate).WithCream().WithCinnamon().Make()
+	hotChocolateWithCreamAndCinnamon = beverage_machine.NewBeverageMachine().HotChocolate().WithCream().WithCinnamon().Make()
 	assertt.InDelta(1.65, hotChocolateWithCreamAndCinnamon.Price(), precision)
 }
